3003bc587abae131a14d0a8dc8a6e492ca0b8e01
GSPTeck <gspteck@gmail.com>
2025-08-15 11:16:34 +0200

    random edits

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -2,45 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-//  //  Auth Endpoints
-//  Register a new account
-Route::post('/api/register', function () {
-
-});
-
-//  Login to existing account
-Route::post('/api/login', function () {
-
-});
-
-//  Get current user data
-Route::get('/api/user', function () {
-
-});
-
-
-//  //  Task Endpoints
-//  Get all tasks
-Route::get('/api/tasks', function () {
-
-});
-
-//  Create a new task
-Route::post('/api/tasks', function () {
-
-});
-
-//  Get a specific task using task_id
-Route::get('/api/tasks/{task_id}', function () {
-
-});
-
-//  Update a specific task using task_id
-Route::put('/api/tasks/{task_id}', function () {
-
-});
-
-//  Delete a specific task using task_id
-Route::delete('/api/tasks/{task_id}', function () {
-
+Route::get('/', function () {
+    return view('welcome');
 });
